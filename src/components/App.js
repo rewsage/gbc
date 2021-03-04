8ebfe423eaa6f963f90ee3346a4bee9462c304b3
Rewsage <rewsage@gmail.com>
2021-03-04 13:13:22 +0300

    initial configuration

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render} from 'react-dom'
+import './assets/css/App.scss'
+
+function App() {
+    return (
+        <div></div>
+    );
+}
+
+render(<App/>, document.getElementById('root'));
+
+export default App
