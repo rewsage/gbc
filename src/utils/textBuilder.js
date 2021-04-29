ba2deb637a215ab8c662853781d58b3800b12fa0
lenix <lenixreviakin@gmail.com>
2021-04-29 18:48:12 +0300

    Updated treeBuilder

--- src/utils/textBuilder.js
+++ src/utils/textBuilder.js
@@ -1,65 +1,72 @@
 import StyleReader from "./StyleReader";
 
+// функция textBuilder строит текст для вывода на сайте в разделе "Export"
 export default function textBuilder(componentName, componentsState) {
-    const componentStyle = componentsState[componentName];
-    const styleReaderCard = new StyleReader(componentStyle);
-    let className = styleReaderCard.className;
     let text;
 
-    const Button = styleReaderCard.button;
-    const styleReaderButton = new StyleReader(componentsState[Button]);
-    let classButton = styleReaderButton.className;
-    let buttonText = componentsState[Button].text;
-    let buttonTag;
-    let classText = isClassEmpty(classButton);
+    const componentStyle = componentsState[componentName];
+    let componentText = componentStyle.text ? '\n\t' + componentStyle.text : '';
 
-    if (buttonText === '' || buttonText === undefined) {
-        buttonTag = `<${Button}${classText}/>`;
-    } else {
-        buttonTag = `<${Button}${classText}>\n\t\t${buttonText}\n\t</${Button}>`;
-    }
+    const styleReader = new StyleReader(componentStyle);
+    let className = styleReader.className;
+    const Button = styleReader.button;
+
+    let classText;
 
     if (componentName === "Card") {
-        const componentText = componentStyle.text ? '\n\t' + componentStyle.text : '';
-        const src = styleReaderCard.img;
+        const src = styleReader.img;
+        // удаляем ненужные параметры, которые будут передаваться отдельным пропсом или как children
         className = className.replace(`btn-${Button}`, '').trim();
         className = className.replace(`url-${src}`, '').trim();
 
+        // формируем отдельный пропс для src
         let additionalText = '';
         if (src !== '') {
             additionalText += ` src="${src}"`;
         }
 
         classText = isClassEmpty(className);
-        return text = `<Card${classText}${additionalText}>${componentText}\n\t${buttonTag}\n</Card>`;
+
+        const buttonCode = textBuilder(Button, componentsState);
+
+        text = `<Card${classText}${additionalText}>${componentText}\n\t${buttonCode}\n</Card>`;
     } else if (componentName === 'Entry') {
-        const componentText = componentStyle.text ? '\n\t' + componentStyle.text : '';
         let typeForm = componentsState["Login"].type;
+        // удаляем ненужные параметры, которые будут передаваться как children
         className = className.replace(`btn-${Button}`, '').trim();
 
         let Login = textBuilder('Login', componentsState);
-        let Pass = Login.replace(`type="${typeForm}"`, `type="Password"`)
+        // второе поле в Entry всегда является паролем, поэтому оно отличается только типом
+        let Pass = Login.replace(`type="${typeForm}"`, `type="Password"`);
+        const buttonCode = textBuilder(Button, componentsState);
+
         classText = isClassEmpty(className);
-        return text = `<Entry${classText}>${componentText}\n\t${Login}\n\t${Pass}\n\t${buttonTag}\n</Entry>`;
+
+        text = `<Entry${classText}>${componentText}\n\t${Login}\n\t${Pass}\n\t${buttonCode}\n</Entry>`;
     } else {
         let additionalText = '';
         if (componentName === 'Login') {
             let typeForm = componentStyle.type;
+            // удаляем ненужные параметры, которые будут передаваться отдельным пропсом
             className = className.replace(`type-${typeForm}`, '').trim();
 
+            // формируем отдельный пропс для type
             if (typeForm !== '') {
                 additionalText += ` type="${typeForm}"`;
             }
         }
-        const componentText = componentStyle.text;
-        classText = isClassEmpty(className)
+        classText = isClassEmpty(className);
+        // изменяем componentText, чтобы код кнопки не переносился на новую строку
+        // иначе в составных компонентах код будет выводиться некоректно
+        componentText = componentStyle.text;
 
         if (componentText === '' || componentText === undefined) {
-            return text = `<${componentName}${classText}${additionalText}/>`;
+            text = `<${componentName}${classText}${additionalText}/>`;
         } else {
-            return text = `<${componentName}${classText}${additionalText}>${componentText}</${componentName}>`;
+            text = `<${componentName}${classText}${additionalText}>${componentText}</${componentName}>`;
         }
     }
+    return text;
 }
 
 function isClassEmpty(className) {
