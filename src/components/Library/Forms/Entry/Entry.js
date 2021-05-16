afc682508dac583a4dedb56a1bf1926938972e5f
Rewsage <rewsage@gmail.com>
2021-05-16 16:46:13 +0300

    Debug EntryForm and fix tree highlight

--- src/components/Library/Forms/Entry/Entry.js
+++ src/components/Library/Forms/Entry/Entry.js
@@ -28,7 +28,7 @@ class Entry extends Component {
         }
 
         return (
-            <form className={styleReader.userClassName + " entry"}
+            <div className={styleReader.userClassName + " entry"}
                   style={styleReader.style}>
                 <h1 className={"entry__title"}>{entryName}</h1>
                 <Authorization componentsState={componentsState}
@@ -39,7 +39,7 @@ class Entry extends Component {
                         componentName={buttonName}>
                     {buttonText}
                 </Button>
-            </form>
+            </div>
         )
     }
 }
